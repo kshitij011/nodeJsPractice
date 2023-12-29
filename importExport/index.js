cefec0ab8d6098646c51cb5a11e9c6ed838c5b6f
Kshitij <kshitijbhoite011@gmail.com>
2023-12-29 15:37:21 +0530

    import export modules

---
+++ importExport/index.js
@@ -0,0 +1,6 @@
+const oper = require('./oper')
+
+console.log(oper.add(5,4));
+console.log(oper.sub(5,4));
+console.log(oper.mul(5,4));
+console.log(oper.name);
---
+++ importExport/oper.js
@@ -0,0 +1,15 @@
+function add(a, b){
+    return a+b;
+}
+
+function sub(a, b){
+    return a-b;
+}
+
+function mul(a, b){
+    return a*b;
+}
+
+const name = "Computer"
+
+module.exports = {add, sub, mul, name}
